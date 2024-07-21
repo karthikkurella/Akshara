cbf08bc5b501c66a5b73fa169adce03c49f84b94
K@rk <krk@hpe-flw4ymf4r4.lan>
2024-07-20 21:59:15 -0400

    initial commit

---
+++ notebooks/app.py
@@ -0,0 +1,108 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+
+from langchain.text_splitter import CharacterTextSplitter
+# from langchain_openai import OpenAI, OpenAIEmbeddings
+from langchain.embeddings import OllamaEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOllama
+
+from webtemplate import css, bot_template, user_template
+
+
+def get_pdf_content(documents):
+    raw_text = ""
+
+    for document in documents:
+        pdf_reader = PdfReader(document)
+        for page in pdf_reader.pages:
+            raw_text += page.extract_text()
+
+    return raw_text
+
+
+def get_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    text_chunks = text_splitter.split_text(text)
+    return text_chunks
+
+
+def get_embeddings(chunks):
+    embeddings = OllamaEmbeddings()
+    # embeddings = HuggingFaceInstructEmbeddings(model_name="")
+    vector_storage = FAISS.from_texts(texts=chunks, embedding=embeddings)
+
+    return vector_storage
+
+
+def start_conversation(vector_embeddings):
+    llm = ChatOllama()
+    memory = ConversationBufferMemory(
+        memory_key='chat_history',
+        return_messages=True
+    )
+    conversation = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vector_embeddings.as_retriever(),
+        memory=memory
+    )
+
+    return conversation
+
+
+def process_query(query_text):
+    response = st.session_state.conversation({'question': query_text})
+    st.session_state.chat_history = response["chat_history"]
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i%2 == 0:
+            st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+
+
+def main():
+
+    st.set_page_config(page_title="Chat with PDFs", page_icon=":books:", layout="wide")
+
+    st.write(css, unsafe_allow_html=True)
+
+    st.header("Hi, I am Akshara, a PDF ChatBot")
+    query = st.text_input("How can I help you today?")
+
+    if query:
+        process_query(query)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
+
+    with st.sidebar:
+        st.subheader("PDF documents")
+        documents = st.file_uploader(
+            "Upload your PDF files", type=["pdf"], accept_multiple_files=True
+        )
+        if st.button("Run"):
+            with st.spinner("Processing..."):
+                # extract text from pdf documents
+                extracted_text = get_pdf_content(documents)
+                # convert text to chunks of data
+                text_chunks = get_chunks(extracted_text)
+                # create vector embeddings
+                vector_embeddings = get_embeddings(text_chunks)
+                # create conversation
+                st.session_state.conversation = start_conversation(vector_embeddings)
+
+
+if __name__ == "__main__":
+    main()
